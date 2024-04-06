840de8d45c8948ff949944b1306c2defe3c8c65c
marlsen <marlsenwang@hotmail.com>
2024-04-06 15:12:34 +0800

    ky

--- tools/git/git_configuration.sh
+++ tools/git/git_configuration.sh
@@ -3,7 +3,4 @@ git config --global user.name "name"
 git config --global init.defaultBranch main
 git config --global core.quotepath false
 git config --global pull.rebase true
-git config --global credential.helper store             // setting global credential store
 # git config core.protectNTFS false
-
-
