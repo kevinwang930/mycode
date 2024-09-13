f83609f174bef71c4f6c41e03d733cae231c7852
marlsen <marlsenstone@hotmail.com>
2024-09-13 11:16:14 +0800

    ky

--- tools/git/git_configuration.sh
+++ tools/git/git_configuration.sh
@@ -4,3 +4,10 @@ git config --global init.defaultBranch main
 git config --global core.quotepath false
 git config --global pull.rebase true
 # git config core.protectNTFS false
+
+
+# auth key generation
+
+```
+ssh-keygen -t ed25519 -C "marlsen@hwxm.com"
+```
